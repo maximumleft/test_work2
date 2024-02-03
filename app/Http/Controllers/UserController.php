4cc42af32f86ae90c226c7bbabb8531235d02b9a
mlevsha <mlevsha@team.amocrm.com>
2024-02-03 14:32:10 +0300

    add UserApi

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\User\StoreRequest;
+use App\Http\Requests\User\UpdateRequest;
+use App\Http\Resources\User\Resource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    public function index(): ResourceCollection
+    {
+        return Resource::collection(User::all());
+    }
+
+    public function show(): Resource
+    {
+        return new Resource(auth()->user());
+    }
+
+    public function store(StoreRequest $request): Resource
+    {
+        $user = User::create($request->validated());
+        auth()->login($user);
+
+        return new Resource($user);
+    }
+
+    public function update(UpdateRequest $request,User $user): Resource
+    {
+        $user->update($request->validated());
+
+        return new Resource($user);
+    }
+
+    public function destroy(User $user): Response
+    {
+        $user->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ app/Http/Requests/User/StoreRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'string|nullable',
+            'username' => 'string|required|max:255|regex:/^[a-zA-Z0-9]+$/i|unique:users',
+            'email' => 'string|required|email|unique:users',
+            'password' => 'string|required|confirmed',
+        ];
+    }
+}
