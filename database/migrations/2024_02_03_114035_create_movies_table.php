979e313f6338fe121f24cb3cff3584c4a2f69c3e
mlevsha <mlevsha@team.amocrm.com>
2024-03-20 15:49:25 +0300

    add docker

--- database/migrations/2024_02_03_114035_create_movies_table.php
+++ database/migrations/2024_02_03_114035_create_movies_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->text('name')->unique();
+            $table->string('name')->unique();
             $table->unsignedInteger('budgetInMillions');
         });
     }
